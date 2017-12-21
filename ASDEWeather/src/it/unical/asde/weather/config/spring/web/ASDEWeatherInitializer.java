f6a86c642c521cb8b53560a2c22a06e72560f0e6
Marco Anastasio <Marco_Anastasio@libero.it>
2017-12-21 11:22:31 +0100

    Added java-based web.xml

---
+++ ASDEWeather/src/it/unical/asde/weather/config/spring/web/ASDEWeatherInitializer.java
@@ -0,0 +1,32 @@
+package it.unical.asde.weather.config.spring.web;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import it.unical.asde.weather.config.spring.security.MultiHttpSecurityConfig;
+
+
+/**
+ * 
+ * @author haze
+ * This class substitute the web.xml file, here every module and Servlets have to be registered 
+ */
+
+public class ASDEWeatherInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {MultiHttpSecurityConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+	
+		return new Class[] {DispatcherServlet.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
