719598bf8377441bfb85bb2ace158c57b0bb88e5
Marco Anastasio <Marco_Anastasio@libero.it>
2017-12-21 11:24:32 +0100

    Added security initializer class

---
+++ ASDEWeather/src/it/unical/asde/weather/config/spring/security/SecurityAppInitializer.java
@@ -0,0 +1,13 @@
+package it.unical.asde.weather.config.spring.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+
+/**
+ * 
+ * @author haze
+ * Initialize the security module on spring 
+ * { https://docs.spring.io/spring-security/site/docs/5.0.0.RELEASE/reference/htmlsingle/#abstractsecuritywebapplicationinitializer-with-spring-mvc }
+ */
+public class SecurityAppInitializer extends AbstractSecurityWebApplicationInitializer{}
