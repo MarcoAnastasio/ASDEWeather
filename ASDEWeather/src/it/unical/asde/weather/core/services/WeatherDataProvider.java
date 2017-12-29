53fe3e023cec541d166e5717e49b05f5f2e02002
Marco <marcuzzuhp@gmail.com>
2017-12-29 14:23:58 +0100

    LoginRequestDTO

--- ASDEWeather/src/it/unical/asde/weather/core/services/WeatherDataProvider.java
+++ ASDEWeather/src/it/unical/asde/weather/core/services/WeatherDataProvider.java
@@ -1,8 +1,12 @@
 package it.unical.asde.weather.core.services;
 
+import java.util.List;
+
 import it.unical.asde.weather.model.bean.comunication.request.RequestListCities;
 import it.unical.asde.weather.model.bean.comunication.request.RequestSingleCity;
+import it.unical.asde.weather.model.bean.geographical.City;
 import it.unical.asde.weather.model.exception.ASDECustomException;
+import it.unical.asde.weather.model.openweatherapi.response.APICurrentResponse;
 
 public interface WeatherDataProvider {
 
@@ -11,5 +15,7 @@ public interface WeatherDataProvider {
 	public Object getCurrentWeatherByCities(RequestListCities request) throws ASDECustomException;
 	
 	public Object getForecastWeatherByCity(RequestSingleCity request) throws ASDECustomException;
+
+	public APICurrentResponse getCurrentWeatherByCities(List<City> cities) throws ASDECustomException;
 	
 }
