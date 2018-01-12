0b5f1b4b411ee3b5312ec495effe4258eb5baf62
Marco Anastasio <Marco_Anastasio@libero.it>
2018-01-12 11:48:30 +0100

    Added angularJs directive

--- ASDEWeather/WebContent/resources/js/app.js
+++ ASDEWeather/WebContent/resources/js/app.js
@@ -5,10 +5,18 @@ var App = angular.module('myApp',[]);
 
 angular.module('cookieStoreExample', ['ngCookies'])
 .controller('UserController', ['$cookieStore', function($rootScope,$cookieStore) {
-  // Put cookie
-  $cookieStore.put('myFavorite','oatmeal');
-  // Get cookie
-  var favoriteCookie = $cookieStore.get('myFavorite');
-  // Removing a cookie
-  $cookieStore.remove('myFavorite');
-}]);
+//	Put cookie
+	$cookieStore.put('myFavorite','oatmeal');
+//	Get cookie
+	var favoriteCookie = $cookieStore.get('myFavorite');
+//	Removing a cookie
+	$cookieStore.remove('myFavorite');
+}])
+
+App.directive('emitLastRepeaterElement', function() {
+	return function(scope,element) {
+
+			angualrFinished();
+
+	};
+});
