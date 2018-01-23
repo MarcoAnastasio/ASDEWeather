0a688688572d323a346f26c15168707ebf2e3df7
Marco <marcuzzuhp@gmail.com>
2018-01-23 23:56:45 +0100

    H2 version

--- ASDEWeather/resources/hibernate_import/importTestData.sql
+++ ASDEWeather/resources/hibernate_import/importTestData.sql
@@ -1,9 +1,9 @@
+
+
 insert into User (id,username,password,firstName, lastName,email) values (NULL, 'ciccio','$2a$10$Vg7o.6MLz6NWOnsGlY16T..B7bWY5ybBco9IRGokOx.kMiuN7tL/e','testName','testLastName','email@test.com');
-
-insert into User_City (User_id,preferedCities_id) values (1,2524907), (1,2523630);
-
-
-insert into City (id,name,latitude,longitude, country_id) values (2022572, 'Khatanga',40.833328,14.25,(	select id from country	where code='RU')	);
+insert into User (id,username,password,firstName, lastName,email) values (NULL, 'pippo','$2a$10$U7YtYDkvhrXjdJ4DuuSWJektqqJ1DyApJLpFo8ZiAQD2U2anOPRYa','pippoName','pippoSurname','pippo@mail.com');
+insert into User (id,username,password,firstName, lastName,email) values (NULL, 'pluto','$2a$10$j44wV2nWOKgA8bi7DTmD7uqLewNNrk8F7G1Qm2PfeSYjo0/jzrRGa','plutoNAme','plutoSurname','pluto@gmail.com');
 
 
-insert into User_City (User_id,preferedCities_id) values  (1,2022572);
+insert into User_City (User_id,preferedCities_id) values (1,	1007311),(	1,	3899539),	(1,	3440939),(	1	,3445690),(1,	2643743),(	1,	2022572),(	1,	2523630),(	1,	2524907),(1,	606507),(2,	3173435),(2,	3128760),(	2,	1819729);
+
