4f6e6b9f6fbb603afbeaeec9d2d731cc739c2821
Marco Anastasio <Marco_Anastasio@libero.it>
2018-01-23 17:11:28 +0100

    Re-added basic missing values managment

--- ASDEWeather/WebContent/resources/js/controller/ForcastWeatherDecoder.js
+++ ASDEWeather/WebContent/resources/js/controller/ForcastWeatherDecoder.js
@@ -2,6 +2,28 @@
  * Author:Abel Mesfin
  */
 
+var directions = [ "",
+	"N",
+	"NNE",
+	"NE",
+	"ENE",
+	"E",
+	"ESE",
+	"SE",
+	"SSE",
+	"S",
+	"SSW",
+	"SW",
+	"WSW",
+	"W",
+	"WNW",
+	"NW",
+	"NNW",
+	"N"
+	];
+
+
+
 
 function Forecasts(forecasts_input,uvdata) {
 	var forecastList = []
@@ -11,7 +33,7 @@ function Forecasts(forecasts_input,uvdata) {
 	}
 	this.forecastList = forecastList;
 	if(uvdata != null)
-	this.uvdata= uvdata.value;
+		this.uvdata= uvdata.value;
 }
 
 function ForecastWeatherDecoder(forecastWeather_input) {
@@ -21,6 +43,7 @@ function ForecastWeatherDecoder(forecastWeather_input) {
 	this.mainTemp = new MainTempDecoder(forecastWeather_input.mainTemperature);
 	this.weather = new WeatherDecoder(forecastWeather_input.weather);
 	this.wind = new WindDecoder(forecastWeather_input.wind);
+	this.cloud= forecastWeather_input.clouds;
 	this.city=new CityDecoder(forecastWeather_input.city);
 };
 
@@ -48,66 +71,77 @@ function WeatherDecoder(weather_input) {
 }
 function WindDecoder(wind_input) {
 	this.speed = wind_input.speed;
-	this.deg = wind_input.deg;
+	this.deg = CalculateCompassDirection(wind_input.deg);
 
 }
+
+
+function CalculateCompassDirection(wind_degree){
+	var Index = wind_degree % 360;
+	Index = Math.round(Index/ 22.5,0) +1;
+	return directions[Index];
+}
+
+
+
+
 function CityDecoder(city_input){
-this.name=city_input.name;
-this.id=city_input.id;
-this.latitude=city_input.latitude;
-this.longitude=city_input.longitude;
-this.country=new CountryDeoder(city_input.country);
+	this.name=city_input.name;
+	this.id=city_input.id;
+	this.latitude=city_input.latitude;
+	this.longitude=city_input.longitude;
+	this.country=new CountryDeoder(city_input.country);
 }
 
 function CountryDeoder(country_input){
 	if(country_input!=null){
-	this.name=country_input.name;
-	this.id=country_input.id;
-	this.code=country_input.code;}
+		this.name=country_input.name;
+		this.id=country_input.id;
+		this.code=country_input.code;}
 }
 
 function DayNamer(day, cond) {
 	if(!null){
-	days_name = [ "Sunday", "Monday", "Tuesday", "Wedensday", "Thursday",
+		days_name = [ "Sunday", "Monday", "Tuesday", "Wedensday", "Thursday",
 			"Friday", "Saturday" ];
-	days_short_name = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
-	var d = new Date(day);
-	return cond ? days_name[d.getDay()] : days_short_name[d.getDay()];}
+		days_short_name = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+		var d = new Date(day);
+		return cond ? days_name[d.getDay()] : days_short_name[d.getDay()];}
 }
 
 function dateDecoder(date_input) {
 	if(date_input!=null){
-	this.dateData = date_input;
-	var substrings = [];
-	substrings = date_input.split(" ");
+		this.dateData = date_input;
+		var substrings = [];
+		substrings = date_input.split(" ");
 
-	var dates = [];
-	dates = substrings[0].split("-");
+		var dates = [];
+		dates = substrings[0].split("-");
 
-	var times = [];
-	times = substrings[1].split(":");
+		var times = [];
+		times = substrings[1].split(":");
 
-	var day = dates[2];
-	var month = dates[1]
-	var year = dates[0];
-	var hour = times[0];
-	var minute = times[1];
-	var dayName = DayNamer(month + "/" + day + "/" + year);
+		var day = dates[2];
+		var month = dates[1]
+		var year = dates[0];
+		var hour = times[0];
+		var minute = times[1];
+		var dayName = DayNamer(month + "/" + day + "/" + year);
 
-	this.day = day;
-	this.month = month;
-	this.year = year;
-	this.hour = hour;
-	this.minute = minute;
-	this.times = substrings[1];
-	this.dayName = dayName;
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		this.hour = hour;
+		this.minute = minute;
+		this.times = substrings[1];
+		this.dayName = dayName;
 	}
-	
+
 
 }
 
 function groupBy(array_input, func_input) {
-	
+
 	var groups = {};
 	array_input.forEach(function(o) {
 		var group = JSON.stringify(func_input(o));
@@ -117,7 +151,7 @@ function groupBy(array_input, func_input) {
 	return Object.keys(groups).map(function(group) {
 		return groups[group];
 	})
-	
+
 }
 
 var icons={};
@@ -131,17 +165,17 @@ function decodeIcon(key) {
 
 
 function getValues(obj, key) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getValues(obj[i], key));
-        } else if (i == key) {
-            objects.push(obj[i]);
-        }
-    }
-    return objects;
-    }
+	var objects = [];
+	for (var i in obj) {
+		if (!obj.hasOwnProperty(i)) continue;
+		if (typeof obj[i] == 'object') {
+			objects = objects.concat(getValues(obj[i], key));
+		} else if (i == key) {
+			objects.push(obj[i]);
+		}
+	}
+	return objects;
+}
 
 
 
