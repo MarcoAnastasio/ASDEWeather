effbf9c79cdd1dda60a597abbc2607c8ed78cf1e
Marco Anastasio <Marco_Anastasio@libero.it>
2018-01-23 17:55:08 +0100

    city import

--- ASDEWeather/tempData/loader.py
+++ ASDEWeather/tempData/loader.py
@@ -34,7 +34,7 @@ def writeCityQuery(city):
 
 
 def writeCountryQuery(country):
-        countryName = row[0].replace("'",r"\'")
+        countryName = row[0].replace("'",r" ")
         countryCode = row[1]
         string = ("insert into Country (id,name,code) values("+
                  "NULL"+
@@ -52,7 +52,7 @@ def writeCountryQuery(country):
 
 #City
 with open('city.list.json','r') as f:
-    write_to = open("../resources/hibernate_import/importCities.sql","w")
+    write_to = open("../resources/hibernate_import/importCities_full.sql","w")
     #Load Json to python dictionary
     data = json.load(f)
     visited = []
@@ -63,8 +63,37 @@ with open('city.list.json','r') as f:
             writeCityQuery(city)
     write_to.close()
 
-with open('SimpleCountryList.csv') as f:
-    write_to = open("../resources/hibernate_import/importCountry.sql","w")
-    data = csv.reader(f,delimiter=",")
-    for row in data:
-        writeCountryQuery(row)
+#City
+with open('city.list.json','r') as f:
+    write_to = open("../resources/hibernate_import/importCities_full.sql","w")
+    #Load Json to python dictionary
+    data = json.load(f)
+    visited = []
+    #Loop throught elements
+    #count = 0;
+    for i in data:
+            city = City(i['name'],i['id'],i['country'],i['coord']['lat'],i['coord']['lon'])
+            visited.append(city)
+           # writeCityQuery(city)
+    sortedcity = sorted(visited,key=lambda City: City.cityid)
+    sortedcity = sorted(sortedcity,key=lambda City: City.name)
+
+    visited = []
+    for i in sortedcity:
+
+        if i.name not in visited:
+            visited.append(i.name)
+            writeCityQuery(i)
+        else :
+            continue
+    writeCityQuery(i)
+    write_to.close()
+
+
+
+
+#with open('SimpleCountryList.csv') as f:
+#    write_to = open("../resources/hibernate_import/importCountry.sql","w")
+#    data = csv.reader(f,delimiter=",")
+#    for row in data:
+#        writeCountryQuery(row)
